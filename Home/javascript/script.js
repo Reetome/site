bee903c5e9a34629e6933b3d61cf0e0617ffe1bb
Re-tome <142092173+Reetome@users.noreply.github.com>
2023-12-30 22:15:37 +0900

    Add files via upload

--- Home/javascript/script.js
+++
@@ -1,8 +0,0 @@
-let encodeText;
-let RandomText;
-let RandomInput;
-
-function EncodeExecution(){
-    encodeText = document.getElementById("EncodeInput").value;
-    document.getElementById("EncodeOutput").value=encodeURI(encodeText);
-}
